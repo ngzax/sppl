41e89ea2325f38d262585d80271e8c29df06c6bd
N. G. Zax <ngzax@comcast.net>
2011-06-05 21:13:00 -0400

    Added the ability to set the Page Title

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,12 @@
 class PlayersController < InheritedResources::Base
   actions :create, :destroy, :edit, :index, :new, :show, :update
+  
+  def index
+    index! { @page_title = "All Players" }
+  end
+
+  def show
+    show! { @page_title = "Showing a Player" }
+  end
+
 end

47cf01d7be1d70652be2840b3d9f946ee66761bb
N. G. Zax <ngzax@comcast.net>
2011-11-12 19:59:25 -0500

    reordered the tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,10 +13,10 @@ module ApplicationHelper
   def resource_menu
     xml = Builder::XmlMarkup.new
     xml.div(:id => "ResourceMenu") do
-      xml << resource_tab_link("seasons", "Seasons")
-      xml << resource_tab_link("matches", "Matches")
-      xml << resource_tab_link("games",   "Games")
       xml << resource_tab_link("players", "Players")
+      xml << resource_tab_link("games",   "Games")
+      xml << resource_tab_link("matches", "Matches")
+      xml << resource_tab_link("seasons", "Seasons")
     end
   end
 

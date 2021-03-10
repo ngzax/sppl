862180d98f12aacf0446ac37c1bf3f1d4408882f
Daryl Richter <daryl@ngzax.com>
2021-03-10 16:41:22 -0500

    The first performance enhancement needed. :)

--- app/models/player.rb
+++ app/models/player.rb
@@ -27,17 +27,36 @@ class Player < ActiveRecord::Base
   end
 
   def season_games(season_id)
-    self.season_results(season_id).collect {|result| result.game}
+    Game.joins(:players, :match => :season).where('seasons.id = ? AND players.id = ?', season_id, self.id)
   end
 
   def season_results(season_id)
-    self.results.select {|result| result.game.match.season_id == season_id}
+    Result.joins(:player, :game => [:match => :season]).where('seasons.id = ? AND players.id = ?', season_id, self.id)
   end
 
   def season_total_points(season_id)
-    return 0 if self.season_results(season_id).nil?
-    p = self.scoring_results(season_id).inject(0) {|sum, each| sum + each.points}
-    p.round(2)
+    sql = %Q(
+      WITH scoring_results AS (
+        SELECT
+          ROUND(CAST(10 * (SQRT(game_results.player_count) / SQRT(results.place)) * (1 + LOG10(10 + 0.25)) AS numeric), 2) AS points
+        FROM
+          results
+        INNER JOIN "players" ON "players"."id" = "results"."player_id"
+        INNER JOIN "games" ON "games"."id" = "results"."game_id"
+        INNER JOIN "matches" ON "matches"."id" = "games"."match_id"
+        INNER JOIN "seasons" ON "seasons"."id" = "matches"."season_id"
+        INNER JOIN (SELECT game_id, COUNT(player_id) AS player_count FROM results GROUP BY game_id) AS game_results ON game_results.game_id = games.id
+        WHERE
+          (seasons.id = #{season_id} AND players.id = #{self.id})
+        ORDER BY
+          points DESC
+        LIMIT 6
+      )
+      SELECT SUM(points) AS total_points FROM scoring_results;
+    )
+
+    result = ActiveRecord::Base.connection.execute(sql)
+    result.first['total_points'].to_f
   end
 
   def to_s
